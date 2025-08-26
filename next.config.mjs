636c10ddf07df5eb54fdda2e345b770531b146f2
NiranjanGhising <ghisingniranjan@gmail.com>
2025-08-27 01:15:40 +0545

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Sample-repo',
-  assetPrefix: '/Sample-repo/',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 }
 
 export default nextConfig
